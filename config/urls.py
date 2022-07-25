ed0f6ab4550548d7f3143951c123f912d1c57381
leonardtkt <leonard@tkt.paris>
2022-07-25 18:42:19 +0200

    Add/Get task

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,8 @@ from tasks import views
 
 urlpatterns = [
     path("", views.index, name="home"),
-    path("add-collection", views.add_collection, name="add-collection"),
+    path("add-collection/", views.add_collection, name="add-collection"),
+    path("add-task/", views.add_task, name="add-task"),
+    path("get-task/<int:collection_pk>/", views.get_tasks, name="get-task"),
     path("admin/", admin.site.urls),
 ]
--- tasks/models.py
+++ tasks/models.py
@@ -5,7 +5,18 @@ class Collection(models.Model):
     name = models.CharField(max_length=120)
     slug = models.SlugField()
 
+    @classmethod
+    def get_default_collection(cls):
+        collection, _ = cls.objects.get_or_create(name="Défaut", slug="_default")
+        return collection
+
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     description = models.CharField(max_length=300)
     collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
