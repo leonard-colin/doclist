c0145f48b92d977f5905abd146bcb093ff230f3b
leonardtkt <leonard@tkt.paris>
2022-07-27 16:01:22 +0200

    More dynamic get and add tasks with htmx

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.template.loader import render_to_string
 from django.utils.html import escape
+from django.utils.text import slugify
 
 from tasks.models import Collection, Task
 
@@ -9,9 +11,13 @@ def index(request):
 
     context = {}
 
+    collection_slug = request.GET.get("collection")
     collection = Collection.get_default_collection()
 
-    context["collections"] = Collection.objects.order_by("-slug")
+    if collection_slug:
+        collection = get_object_or_404(Collection, slug=collection_slug)
+
+    context["collections"] = Collection.objects.order_by("slug")
     context["tasks"] = collection.task_set.order_by("description")
 
     return render(request, "tasks/index.html", context=context)
@@ -19,7 +25,7 @@ def index(request):
 
 def add_collection(request):
     collection_name = escape(request.POST.get("collection-name"))
-    collection, created = Collection.objects.get_or_create(name=collection_name)
+    collection, created = Collection.objects.get_or_create(name=collection_name, slug=slugify(collection_name))
     if not created:
         return HttpResponse("La collection existe déjà.", status=409)
 
@@ -28,13 +34,16 @@ def add_collection(request):
 
 def add_task(request):
 
-    collection = Collection.get_default_collection()
-
+    # collection = Collection.get_default_collection()
+    collection = Collection.objects.get(slug=request.POST.get("collection"))
     description = escape(request.POST.get("task-description"))
     Task.objects.create(description=description, collection=collection)
     return HttpResponse(description)
 
 
 def get_tasks(request, collection_pk):
+
     collection = get_object_or_404(Collection, pk=collection_pk)
-    collection.task_set.order_by("description")
+    tasks = collection.task_set.order_by("description")
+
+    return render(request, 'tasks/tasks.html', context={"tasks": tasks})
